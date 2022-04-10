37e52e05425ca015c7fcbeecedd9a8995817db87
kostya8994 <kostya8994@yandex.ru>
2022-04-10 18:03:34 +0800

    Исправил значение по умолчанию

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,15 @@
 fun main() {
-    println (transfer("Mastercard", 0.0, 20000.0))
+    println (transfer("Mastercard", kop =20000.0))
     println (transfer("Maestro", 10000.0, 1000000.0))
     println (transfer("Mastercard", 5000000.0, 15000000.0))
     println (transfer("Mastercard", 50000000.0, 15000000.0))
     println (transfer("Visa", 0.0, 100000.0))
     println (transfer("Мир", 500000.0, 500000.0))
     println (transfer("VK Pay", 0.0, 70000.0))
-    println (transfer("VK Pay", 700000.0, 10000000.0))
+    println (transfer( monthlyAmount = 700000.0, kop = 10000000.0))
     println (transfer("123", 700000.0, 10000000.0))
 }
-fun transfer(cardType: String, monthlyAmount: Double, kop: Double): String{
+fun transfer(cardType: String = "VK Pay", monthlyAmount: Double = 0.0, kop: Double): String{
     return when (cardType) {
         "Mastercard", "Maestro" -> transferMastercard(monthlyAmount, kop)
         "Visa", "Мир" -> transferVisa(monthlyAmount, kop)
